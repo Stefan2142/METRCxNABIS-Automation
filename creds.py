3516f5fd226e47dd2d8f9bc1cf7971eadb4318eb
Stefan2142 <stefan_2142@outlook.com>
2022-03-08 22:11:23 +0100

    fixed untracked files

--- creds.py
+++
@@ -1,8 +0,0 @@
-credentials = {
-    "nabis": {"un": "marko@nabis.com", "pwd": "Nabis123!"},
-    "metrc": {"un": "alex@headquarters.co", "pwd": "s9tjpBYiNfCJ*9m"},
-}
-GARDEN_OF_WEEDEN = (
-    "TGljZW5zZWRMb2NhdGlvbjo0MWRiMDk0Ny1kMWYzLTQ5NTUtYWYyMC1kODkyZjE4NjAxMDk="
-)
-NABITWO = "TGljZW5zZWRMb2NhdGlvbjowY2VkY2Y2ZC04Nzg0LTRiNzYtYmU5Yy0wZTEwMmU1MjQ1Zjk="
